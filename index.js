25a01c0a3e0b6cfb2fe499fec1f4bee17f4f5faf
brandonpantp <39585561+brandonpantp@users.noreply.github.com>
2018-05-26 09:25:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -46,18 +46,18 @@ bot.on('message', function (event) {
     //////測試連結Google試算表功能
     
 var GoogleSpreadsheet = require('google-spreadsheet');
-var creds = require('./client_secret.json');
+//var creds = require('./client_secret.json');
  
 // Create a document object using the ID of the spreadsheet - obtained from its URL.
 var doc = new GoogleSpreadsheet('1GjY1OKGyO_QMLTk4G10J_cCpb_rAbKXcMs8Q2aLrHEo');
  
 // Authenticate with the Google Spreadsheets API.
-doc.useServiceAccountAuth(creds, function (err) {
+//doc.useServiceAccountAuth(creds, function (err) {
  
   // Get all of the rows from the spreadsheet.
   doc.getRows(1, function (err, rows) {
     console.log(rows);
-  });
+  //});
 });
     
     //////Line主動推播測試
