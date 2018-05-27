e35678302636cd7881e7a9a9d953923d35ea4b39
Sam Liu <hi1999@gmail.com>
2018-05-27 21:25:53 +0800

    TT

--- index.js
+++ index.js
@@ -2,7 +2,24 @@
 var express = require('express');
 var google = require('googleapis');
 var googleAuth = require('google-auth-library');
-    var xlsx = require('node-xlsx');
+var xlsx = require('node-xlsx');
+
+var http = require('http');
+var querystring = require('querystring');
+var util = require('util');
+
+http.createServer(function (req, res) {
+    var post = '';     
+
+    req.on('data', function (chunk) {    
+        post += chunk;
+    });
+
+    req.on('end', function () {
+        post = querystring.parse(post);
+        res.end(util.inspect(post));
+    });
+}).listen(3000);
 
 var bot = linebot({
     channelId: '1574577182',
