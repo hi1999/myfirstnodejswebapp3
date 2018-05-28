dc4da2a268e03ea48b8e82148c640041b324f67b
Sam Liu <hi1999@gmail.com>
2018-05-28 21:46:02 +0800

    AAA

--- index2.js
+++ index2.js
@@ -9,25 +9,31 @@ var bot = linebot({
     channelAccessToken: 'NZXzzQ3o+VJYjHusBszu5QIoff22qIQ88z+F0fOFeFKIYsLtuYPB4XJAGY84LJIBJpv5pnp8fBrC6kO0rG4bq1detk7Qh40XADbWE524z77Sdumg1Hom12AXpa827FVnCoR81vbtvGoDlHCrX5MdmwdB04t89/1O/w1cDnyilFU='
 });
 
-//事件：加入好友
+//使用者加入機器人好友事件
 bot.on('follow', function (event) {
-    console.log('==================follow');
+    console.log('==================follow-使用者加入機器人好友事件');
 });
+//使用者刪除機器人好友事件
 bot.on('unfollow', function (event) {
-    console.log('==================unfollow');
+    console.log('==================unfollow-使用者刪除機器人好友事件');
 });
-//事件：加入群組
+//機器人加入群組時的事件
 bot.on('join', function (event) { 
-    console.log('==================join');
+    console.log('==================join-機器人加入群組時的事件');
 });
-//事件：退出群組
+//機器人離開群組時的事件
 bot.on('leave', function (event) {
-    console.log('==================leave');
+    console.log('==================leave-機器人離開群組時的事件');
+});;
+//使用者透過套版訊息回應時的事件
+bot.on('postback', function (event) {
+    console.log('==================postback-使用者透過套版訊息回應時的事件');
 });
+//訊息事件
 bot.on('message', function (event) {
+    console.log('==================message-訊息事件');
 
     //把收到訊息的 event 印出來看看
-    console.log('==================');
     console.log('解析收到的event:');
     //console.log(event);
     console.log('type==>', event.type);
@@ -35,15 +41,18 @@ bot.on('message', function (event) {
     console.log('userId==>', event.source.userId);
     console.log('==================');
 
-    if (event.type === 'message') {
-        console.log(event.type, event.message);
-    } else if (event.type === 'follow') {
-        console.log(event.type, '');
-        console.log(event.source);
-    }
-    console.log('==================');
 });
 
+const line = require('@line/bot-sdk');
+const client = new line.Client({
+    channelAccessToken: 'NZXzzQ3o+VJYjHusBszu5QIoff22qIQ88z+F0fOFeFKIYsLtuYPB4XJAGY84LJIBJpv5pnp8fBrC6kO0rG4bq1detk7Qh40XADbWE524z77Sdumg1Hom12AXpa827FVnCoR81vbtvGoDlHCrX5MdmwdB04t89/1O/w1cDnyilFU='
+});
+client.leaveGroup('<groupId>').then(() => {
+    console.log('12345');
+    })
+    .catch((err) => {
+        // error handling
+    });
 
 const app = express();
 const linebotParser = bot.parser();
