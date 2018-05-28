14cc22c983a329f64a4eb7b694af8878011ba794
brandonpantp <39585561+brandonpantp@users.noreply.github.com>
2018-05-28 18:56:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -48,13 +48,22 @@ MongoClient.connect(url, function (err, database) {
 //    console.log('DB更新資料成功');
 //    database.close();
 //}); 
-bot.on(event.type, function (event) {
+bot.on('follow', function (event) {
 
     //把收到訊息的 event 印出來看看
     console.log('解析收到的event:');
     //console.log(event); 
    // console.log('┌──(參數解析)───────────────');
-   // console.log('event type',event.type);
+    console.log('event type',event.type);
+}
+
+bot.on('message', function (event) {
+
+    //把收到訊息的 event 印出來看看
+    console.log('解析收到的event:');
+    //console.log(event); 
+   // console.log('┌──(參數解析)───────────────');
+    console.log('event type',event.type);
    // console.log('│message', event.message);
    // console.log('│userId', event.source.userId);
    // console.log('│groupId', event.source.groupId);
