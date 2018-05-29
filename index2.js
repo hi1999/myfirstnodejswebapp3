2ba13ccbb98af966da37b588ab501629dcbc8f10
brandonpantp <39585561+brandonpantp@users.noreply.github.com>
2018-05-29 08:10:37 +0800

    Update index2.js

--- index2.js
+++ index2.js
@@ -12,10 +12,19 @@ var bot = linebot({
 //使用者加入機器人好友事件
 bot.on('follow', function (event) {
     console.log('==================follow-使用者加入機器人好友事件');
+    //1.讀取userid
+    //2.判斷是否已存在於資料庫(假設可以建立表格，表格可以有欄位1表{user_id,user_name,start_time,friend,get_times})
+    //  2.1若已存在資料庫，將1表"friend"欄位更新為Yes
+    //  2.2若尚未存在於資料庫，1表新增一筆資料
+    
 });
 //使用者刪除機器人好友事件
 bot.on('unfollow', function (event) {
     console.log('==================unfollow-使用者刪除機器人好友事件');
+    //1.讀取userid
+    //2.於資料庫(假設可以建立表格，表格可以有欄位1表{user_id,user_name,start_time,friend})
+    //  2.1若已存在資料庫，將"friend"欄位更新為No
+
 });
 //機器人加入群組時的事件
 bot.on('join', function (event) { 
@@ -32,7 +41,11 @@ bot.on('postback', function (event) {
 //訊息事件
 bot.on('message', function (event) {
     console.log('==================message-訊息事件');
-
+    //1.判斷使用者傳送的訊息是否為"抽"
+    //  1.1若不為抽，不處理
+    //  1.2若為抽，於表1(假設可以建立表格，表格可以有欄位1表{user_id,user_name,start_time,friend,get_times})將get friends+1
+    //  1.3若為抽，於表2(假設可以建立表格，表格可以有欄位2表{user_id,current_time,get_times})判斷該使用者當日抽圖資料是否已存在，若不存在心件資料，
+    //     若存在將get times+1
     //把收到訊息的 event 印出來看看
     console.log('解析收到的event:');
     //console.log(event);
