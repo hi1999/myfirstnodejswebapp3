8cba2848a835f2febcb7be7f220c3fc4bb0009cb
brandonpantp <39585561+brandonpantp@users.noreply.github.com>
2018-05-30 07:39:24 +0800

    Update index3.js

--- index3.js
+++ index3.js
@@ -21,53 +21,62 @@ const client = new Client({
 client.connect();
 console.log('連線OK');
 
-//query all table in this database ==>OK
-//console.log('query all table in this database');
-//client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-//    if (err) throw err;
-//    for (let row of res.rows) {
-//        console.log(JSON.stringify(row));
-//    }
-//    //client.end();
-//});
-//bot.on('message', function (event) {
-//insert data ==>OK, 注意userid是KEY，重覆的會INSERT不進去
-//var user_id=event.source.userId;
-console.log('insert table test1');
-client.query(
-    'INSERT into public.test1 (userid, cc, update_at) VALUES($1, $2, $3) ',
-    ['B' + new Date(), 101, new Date()],
-    function (err1, result) {
-        if (err1) throw err1;
-        //client.end();
-    });
-
+//使用者加入機器人好友事件
+bot.on('follow', function (event) {
+    console.log('==================follow-使用者加入機器人好友事件');
+    //1.讀取userid
+    //2.判斷是否已存在於資料庫(假設可以建立表格，表格可以有欄位1表{user_id,user_name,start_time,friend,get_times})
+    //  2.1若已存在資料庫，將1表"friend"欄位更新為Yes
+    //  2.2若尚未存在於資料庫，1表新增一筆資料
     
-//});
-
-//update data
-console.log('update table test1');
-client.query("UPDATE public.test1 SET cc=cc+1 WHERE userid = 'Sam1'", (err2, res) => {
-    if (err2) throw err2;
-    //client.end();
 });
-//client.query(
-//    "UPDATE public.test1 SET cc=102 WHERE userid = 'Sam1' ",
-//    ['Sam2', 101, new Date()],
-//    function (err, result) {
-//        if (err) throw err;
-//        //client.end();
-//    });
+//使用者刪除機器人好友事件
+bot.on('unfollow', function (event) {
+    console.log('==================unfollow-使用者刪除機器人好友事件');
+    //1.讀取userid
+    //2.於資料庫(假設可以建立表格，表格可以有欄位1表{user_id,user_name,start_time,friend})
+    //  2.1若已存在資料庫，將"friend"欄位更新為No
 
-//query table test1 ==>OK
-console.log('query table test1');
-client.query('SELECT * FROM public.test1;', (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-        console.log(JSON.stringify(row));
-    }
-    client.end();
 });
+//機器人加入群組時的事件
+bot.on('join', function (event) { 
+    console.log('==================join-機器人加入群組時的事件');
+});
+//機器人離開群組時的事件
+bot.on('leave', function (event) {
+    console.log('==================leave-機器人離開群組時的事件');
+});;
+//使用者透過套版訊息回應時的事件
+bot.on('postback', function (event) {
+    console.log('==================postback-使用者透過套版訊息回應時的事件');
+});
+//訊息事件
+bot.on('message', function (event) {
+    console.log('==================message-訊息事件');
+    //1.判斷使用者傳送的訊息是否為"抽"
+    //  1.1若不為抽，不處理
+    //  1.2若為抽，於表1(假設可以建立表格，表格可以有欄位1表{user_id,user_name,start_time,friend,get_times})將get friends+1
+    //  1.3若為抽，於表2(假設可以建立表格，表格可以有欄位2表{user_id,current_time,get_times})判斷該使用者當日抽圖資料是否已存在，若不存在心件資料，
+    //     若存在將get times+1
+    //把收到訊息的 event 印出來看看
+    console.log('解析收到的event:');
+    //console.log(event);
+    console.log('type==>', event.type);
+    console.log('replyToken==>', event.replyToken);
+    console.log('userId==>', event.source.userId);
+    console.log('==================');
+
+});
+
+
+
+
+
+
+
+
+
+
 
 
 const app = express();
