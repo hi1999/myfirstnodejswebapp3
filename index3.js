e1973c6d6648d4c22c6d3ababdc1e37735cf92e5
brandonpantp <39585561+brandonpantp@users.noreply.github.com>
2018-05-30 10:38:22 +0800

    Update index3.js

--- index3.js
+++ index3.js
@@ -32,7 +32,7 @@ bot.on('follow', function (event) {
     console.log('insert table test1');
    
     console.log('query table test1');
-client.query("SELECT * FROM public.test1 where userid='Brandon';", (err, res) => {
+client.query("SELECT * FROM public.test1 where userid='Sam';", (err, res) => {
 
     if (err) throw err;
 
