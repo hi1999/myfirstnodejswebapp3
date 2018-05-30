feff15eaa6d99b401505ff055831081a0a5892b0
brandonpantp <39585561+brandonpantp@users.noreply.github.com>
2018-05-30 10:06:32 +0800

    Update index3.js

--- index3.js
+++ index3.js
@@ -39,8 +39,8 @@ client.query("SELECT count(*) FROM public.test1 where userid='Brandon';", (err, 
     for (let row of res.rows) {
         var test=row.count;
        // console.log(test);
-        if(test=='1'){
-        console.log(test);
+        if(test=="1"){
+        console.log("test:"+test);
         }
         //console.log(JSON.stringify(row));
     }
