4d6cb29d6e984a8361878cde173033eb35daf03a
brandonpantp <39585561+brandonpantp@users.noreply.github.com>
2018-06-01 09:17:39 +0800

    Update schedule_task.js

--- schedule_task.js
+++ schedule_task.js
@@ -37,7 +37,44 @@ const ME = 'Ubb9f5c58d8fc3755bc871dcda17439f6';
             }]
         }
 });*/
-///////////
+///////////下面推播template測試OK
+/*bot.push(ME, {
+ "type": "template",
+  "altText": "This is a buttons template",
+  "template": {
+      "type": "buttons",
+      "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+      "imageAspectRatio": "rectangle",
+      "imageSize": "cover",
+      "imageBackgroundColor": "#FFFFFF",
+      "title": "Menu",
+      "text": "Please select",
+      "defaultAction": {
+          "type": "uri",
+          "label": "View detail",
+          "uri": "http://example.com/page/123"
+      },
+      "actions": [
+          {
+            "type": "postback",
+            "label": "Buy",
+            "data": "action=buy&itemid=123"
+          },
+          {
+            "type": "postback",
+            "label": "Add to cart",
+            "data": "action=add&itemid=123"
+          },
+          {
+            "type": "uri",
+            "label": "View detail",
+            "uri": "http://example.com/page/123"
+          }
+      ]
+  }
+});
+*/
+
 bot.push(ME, {
  "type": "template",
   "altText": "This is a buttons template",
