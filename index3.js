b434a105b0b5a6b24eb9d502fb01f1c7674afda9
brandonpantp <39585561+brandonpantp@users.noreply.github.com>
2018-06-02 09:46:27 +0800

    Update index3.js

--- index3.js
+++ index3.js
@@ -133,9 +133,9 @@ bot.on('message', function (event) {
                     iFriend=row.count;
                     console.log(JSON.stringify(row));
                     event.reply("超過30次抽的人數:"+iCOUNT+"人"+"\n"+
-                                "      抽的總次數:"+iSUM+"次"+"\n"+
-                                "    目前訂閱人數:"+iFriend+"人"+"\n"+
-                                "  活躍用戶比率為:"+iCOUNT/iFriend*100+"%"
+                                "        抽的總次數:"+iSUM+"次"+"\n"+
+                                "      目前訂閱人數:"+iFriend+"人"+"\n"+
+                                "   活躍用戶比率為:"+iCOUNT/iFriend*100+"%"
                                ).then(function (data) {
                         console.log('success', data);
                     }).catch(function (error) {
