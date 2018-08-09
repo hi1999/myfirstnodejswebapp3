2a2580647e75eb703262f615e487caa6a80f722e
Sam Liu <hi1999@gmail.com>
2018-08-09 21:27:38 +0800

    a

--- gatest1.js
+++ gatest1.js
@@ -9,11 +9,11 @@ app.get("/", function (req, res) {
     res.send('Hello World!')
 });
 
-//app.get("/test", function (req, res) {
-//    visitor.pageview("/somepage", "测试页面", "https://luolei.org").send();
-//    visitor.event("事件类别", "事件行为", "事件标签", 42).send();
-//    res.redirect("http://this.is26.com");
-//});
+app.get("/test", function (req, res) {
+    visitor.pageview("/somepage", "测试页面", "https://luolei.org").send();
+    visitor.event("事件类别", "事件行为", "事件标签", 42).send();
+    res.redirect("http://tw.yahoo.com");
+});
 
 //app.listen(3000);
 
