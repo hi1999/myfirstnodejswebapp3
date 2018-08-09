ba0eb3a8214ee9d8f28ead86ce9571cdadc32594
Sam Liu <hi1999@gmail.com>
2018-08-09 21:15:16 +0800

    A

--- gatest1.js
+++ gatest1.js
@@ -1,1 +1,18 @@
-﻿
+﻿'use strict'
+var express = require('express');
+var app = express();
+
+var ua = require('universal-analytics');
+var visitor = ua('UA-21856187-7');//谷歌统计
+
+app.get("/", function (req, res) {
+    res.send('Hello World!')
+});
+
+app.get("/test", function (req, res) {
+    visitor.pageview("/somepage", "测试页面", "https://luolei.org").send();
+    visitor.event("事件类别", "事件行为", "事件标签", 42).send();
+    res.redirect("http://this.is26.com");
+});
+
+app.listen(3000);
