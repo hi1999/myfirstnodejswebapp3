7b21ce4ead738483a19200b51a85bbdbfc3247f9
Sam Liu <hi1999@gmail.com>
2018-08-10 14:39:44 +0800

    Update gatest1.js

--- gatest1.js
+++ gatest1.js
@@ -6,9 +6,14 @@ const credentials = require('./service-account.json')
 const SPREADSHEET_ID = '1GrvkbHdttGR8cG3M494MIqj5TvwNBts3Miy0ZszdQLo';
 
 async function accessSpreadsheet() {
+  console.log('-----1');
   const doc = new GoogleSpreadsheet(SPREADSHEET_ID)
+  console.log('-----2');
   await promisify(doc.useServiceAccountAuth)(creds)
+  console.log('-----3');
   const info = await promisify(doc.getInfo)()
+  
+  console.log('-----4');
   console.log('Loaded doc: ' + info.title +' by '+ info.author.email)
   const sheet = info.worksheets[0]
   console.log(
