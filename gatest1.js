b1f68ffe5c4a5c800ff5a0d90774da1fa8d5ab85
Sam Liu <hi1999@gmail.com>
2018-08-10 13:42:26 +0800

    Update gatest1.js

--- gatest1.js
+++ gatest1.js
@@ -30,6 +30,45 @@ function authorize(credentials, callback) {
     console.log('---------------------------authorize#');
 }
 
+function getNewToken(oAuth2Client, callback) {
+    console.log('---------------------------getNewToken');
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES,
+    });
+    console.log('---------------------------authUrl:' + authUrl);
+    console.log('Authorize this app by visiting this url:', authUrl);
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    console.log('---------------------------rl.output' + rl.output);
+
+    oAuth2Client.getToken(apiKey, (err, token) => {
+        if (err) return console.error('Error while trying to retrieve access token', err);
+        oAuth2Client.setCredentials(token);
+        // Store the token to disk for later program executions
+        fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+            if (err) console.error(err);
+            console.log('Token stored to', TOKEN_PATH);
+        });
+        callback(oAuth2Client);
+    });
+    //rl.question('Enter the code from that page here: ', (code) => {
+    //    rl.close();
+    //    oAuth2Client.getToken(code, (err, token) => {
+    //        if (err) return console.error('Error while trying to retrieve access token', err);
+    //        oAuth2Client.setCredentials(token);
+    //        // Store the token to disk for later program executions
+    //        fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+    //            if (err) console.error(err);
+    //            console.log('Token stored to', TOKEN_PATH);
+    //        });
+    //        callback(oAuth2Client);
+    //    });
+    //});
+}
+
 function ReadGoogleSheet(auth) {
     console.log('---------------------------ReadGoogleSheet');
     const sheets = google.sheets({ version: 'v4', auth });
