b2e553ce92e24679957efc36759e7940c678bce9
Sam Liu <hi1999@gmail.com>
2018-08-10 13:19:28 +0800

    Update gatest1.js

--- gatest1.js
+++ gatest1.js
@@ -1,7 +1,9 @@
 var express = require('express');
 const app = express();
+
 app.get("/", function (req, res) {
     res.send('Hello World!')
+    console.log('Hello World!');
 });
 
 //app.listen(3000);
