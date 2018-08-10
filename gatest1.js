efc8b81fae813001a424f0476709c7b7e913ee81
Sam Liu <hi1999@gmail.com>
2018-08-10 13:26:24 +0800

    Update gatest1.js

--- gatest1.js
+++ gatest1.js
@@ -1,4 +1,15 @@
 var express = require('express');
+var fs = require('fs');
+
+fs.readFile('credentials.json', (err, content) => {
+    var str = content.toString();
+    if (err) return console.log('Error loading client secret file:', err);
+    // Authorize a client with credentials, then call the Google Sheets API.
+    //authorize(JSON.parse(content), listMajors);
+
+    console.log(str.length);
+    console.log('finish');
+});
 const app = express();
 
 app.get("/", function (req, res) {
